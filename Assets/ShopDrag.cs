9af430a2dd82762f81579a20aba39fbc5d6990b7
Cristofer Oswald <cristoferoswald@gmail.com>
2015-12-10 18:35:51 -0200

    Almost fixed drag and drop

--- Assets/ShopDrag.cs
+++ Assets/ShopDrag.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
 
-public class ShopDrag : MonoBehaviour {
+public class ShopDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
-    public void move(GameObject obj){
-        Vector2 mos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Debug.Log("mouse x" +Input.mousePosition.x.ToString());
-        Debug.Log("mouse y" + Input.mousePosition.y.ToString());
-        Debug.Log("x = " + obj.transform.localPosition.x.ToString());
-        Debug.Log("y = " + obj.transform.localPosition.y.ToString());
-        obj.transform.localPosition = mos;
+    public void OnBeginDrag(PointerEventData eventData){
     }
-	
+
+    public void OnDrag(PointerEventData eventData){
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+    }
+
 }
+
+/*
+ Debug.Log("this.x = " +this.transform.localPosition.x.ToString());
+        Debug.Log("this.y = " +this.transform.localPosition.y.ToString());
+        Debug.Log("this.z = " +this.transform.localPosition.z.ToString());
+        Debug.Log("mouse x = " + eventData.position.x.ToString());
+        Debug.Log("mouse y = " + eventData.position.y.ToString());
+Debug.Log("mouse new x  = " + pos.x.ToString());
+        Debug.Log("mouse new y  = " + pos.y.ToString());
+        Debug.Log("mouse new z  = " + pos.z.ToString());
+        */
